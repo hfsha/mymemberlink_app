4efabef84cf03161135dc504fbb74c8741925096
hfsha <hfshahidatul.uum@gmail.com>
2024-11-10 18:53:13 +0800

    latest

--- memberlink_app/server/register.php
+++ memberlink_app/server/register.php
@@ -11,22 +11,29 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 $password = sha1($_POST['password']);
 
-$sqlinsert="INSERT INTO `user`(`fullname`, `phone`, `email`, `password`) VALUES ('$fullname','$phone','$email','$password')";
+// Check if the email already exists
+$sqlcheck = "SELECT * FROM `user` WHERE `email` = '$email'";
+$result = $conn->query($sqlcheck);
+
+if ($result->num_rows > 0) {
+	$response = array('status' => 'email_exists', 'data' => null);
+    sendJsonResponse($response);
+    die;
+}
+
+// If the email does not exist, proceed with the registration
+$sqlinsert = "INSERT INTO `user`(`fullname`, `phone`, `email`, `password`) VALUES ('$fullname','$phone','$email','$password')";
 
 if ($conn->query($sqlinsert) === TRUE) {
 	$response = array('status' => 'success', 'data' => null);
     sendJsonResponse($response);
-}else{
+} else {
 	$response = array('status' => 'failed', 'data' => null);
 	sendJsonResponse($response);
 }
-	
-	
 
-function sendJsonResponse($sentArray)
-{
+function sendJsonResponse($sentArray) {
     header('Content-Type: application/json');
     echo json_encode($sentArray);
 }
-
 ?>
